44dd783b29f8da6b56bd2d897254a04fbbced278
shivaji0234 <nampallisivaji@gmail.com>
2025-10-23 19:34:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const navBarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navBarToggle.addEventListener('click', () => {
+    navBarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
